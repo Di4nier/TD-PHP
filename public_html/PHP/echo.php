498d6fcfd98b06d307f184f73c5a1454dc1822f6
Di4nier <98035490+Di4nier@users.noreply.github.com>
2022-05-17 16:51:54 +0200

    Add files via upload

---
+++ public_html/PHP/echo.php
@@ -0,0 +1,24 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page 1 PHP</title>
+</head>
+<body>
+    Voici le résultat du script PHP : 
+        <?php
+          // Ceci est un commentaire PHP sur une ligne
+          /* Ceci est le 2ème type de commentaire PHP
+          sur plusieurs lignes */
+        
+          // On met la chaine de caractères "hello" dans la variable 'texte'
+          // Les noms de variable commencent par $ en PHP
+          $texte = "hello world !";
+
+          // On écrit le contenu de la variable 'texte' dans la page Web
+          echo $texte;
+        ?>
+    
+</body>
+</html>
